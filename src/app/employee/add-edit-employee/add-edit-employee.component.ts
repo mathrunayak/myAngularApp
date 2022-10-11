b49728e19e1f139196a90bf42ceb00c1c7a2ab55
Mathru Nayak <Mathru.N@Orioninc.com>
2022-10-11 11:25:13 +0530

    Created a new Android app

---
+++ src/app/employee/add-edit-employee/add-edit-employee.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ApiserviceService } from 'src/app/apiservice.service';
+
+@Component({
+  selector: 'app-add-edit-employee',
+  templateUrl: './add-edit-employee.component.html',
+  styleUrls: ['./add-edit-employee.component.css']
+})
+export class AddEditEmployeeComponent implements OnInit {
+
+  constructor(private service: ApiserviceService) { }
+  @Input() emp: any;
+  EmployeeId = "";
+  EmployeeName = "";
+  Department = "";
+  DateOfJoining = "";
+  DepartmentList: any = [];
+
+
+  ngOnInit(): void {
+    this.loadEmployeeList();
+  }
+
+  loadEmployeeList() {
+
+    this.service.getAllDepartmentNames().subscribe((data: any) => {
+      this.DepartmentList = data;
+
+      this.EmployeeId = this.emp.EmployeeId;
+      this.EmployeeName = this.emp.EmployeeName;
+      this.Department = this.emp.Department;
+      this.DateOfJoining = this.emp.DateOfJoining;
+    });
+  }
+
+  addEmployee() {
+    var val = {
+      EmployeeId: this.EmployeeId,
+      EmployeeName: this.EmployeeName,
+      Department: this.Department,
+      DateOfJoining: this.DateOfJoining
+    };
+
+    this.service.addEmployee(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
+  updateEmployee() {
+    var val = {
+      EmployeeId: this.EmployeeId,
+      EmployeeName: this.EmployeeName,
+      Department: this.Department,
+      DateOfJoining: this.DateOfJoining
+    };
+
+    this.service.updateEmployee(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
+
+  
+}
